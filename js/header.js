8f529c7b05fe9fc68f3086a08237664719c11756
ReynaldoMolina <molinareynaldo952@gmail.com>
2025-01-10 17:59:27 -0600

    Added portfolio page

--- js/header.js
+++ js/header.js
@@ -3,12 +3,12 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <header class="flx flx-center">
         <div class="header flx flx-center">
-          <a href="#" class="header-logo flx flx-center">
+          <a href="/index.html" class="header-logo flx flx-center">
             <img src="/assets/icons/logo.svg" alt="logo">
           </a>
           <div class="flx">
-            <a href="#" class="header-link">About</a>
-            <a href="#" class="header-link">Portfolio</a>
+            <a href="/index.html" class="header-link">About</a>
+            <a href="/html/portfolio.html" class="header-link">Portfolio</a>
           </div>
         </div>
       </header>
