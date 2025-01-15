62b7becbdb7ce1e46e56428b124e524b699ac0e7
ReynaldoMolina <molinareynaldo952@gmail.com>
2025-01-15 15:24:45 -0600

    added 20 dollar bill

--- src/moneycounter/money.js
+++ src/moneycounter/money.js
@@ -16,26 +16,33 @@ let dollars = [
   {
     id: 2,
     symbol: '$',
-    value: 10,
+    value: 20,
     quantity: 0,
     subtotal: 0
   },
   {
     id: 3,
     symbol: '$',
-    value: 5,
+    value: 10,
     quantity: 0,
     subtotal: 0
   },
   {
     id: 4,
     symbol: '$',
-    value: 2,
+    value: 5,
     quantity: 0,
     subtotal: 0
   },
   {
     id: 5,
+    symbol: '$',
+    value: 2,
+    quantity: 0,
+    subtotal: 0
+  },
+  {
+    id: 6,
     symbol: '$',
     value: 1,
     quantity: 0,
@@ -45,84 +52,84 @@ let dollars = [
 
 let cordobas = [
   {
-    id: 6,
+    id: 7,
     symbol: 'C$',
     value: 1000,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 7,
+    id: 8,
     symbol: 'C$',
     value: 500,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 8,
+    id: 9,
     symbol: 'C$',
     value: 200,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 9,
+    id: 10,
     symbol: 'C$',
     value: 100,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 10,
+    id: 11,
     symbol: 'C$',
     value: 50,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 11,
+    id: 12,
     symbol: 'C$',
     value: 20,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 12,
+    id: 13,
     symbol: 'C$',
     value: 10,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 13,
+    id: 14,
     symbol: 'C$',
     value: 5,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 14,
+    id: 15,
     symbol: 'C$',
     value: 1,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 15,
+    id: 16,
     symbol: 'C$',
     value: 0.5,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 16,
+    id: 17,
     symbol: 'C$',
     value: 0.25,
     quantity: 0,
     subtotal: 0
   },
   {
-    id: 17,
+    id: 18,
     symbol: 'C$',
     value: 0.1,
     quantity: 0,
