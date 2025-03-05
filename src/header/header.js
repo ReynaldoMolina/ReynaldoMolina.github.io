07af477d23dd1bc86b86cc54a7f860e824f78e48
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-03-05 14:21:09 -0600

    Experimenting with styles

--- src/header/header.js
+++ src/header/header.js
@@ -2,9 +2,9 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="header flx flx-center">
-        <a href="/" class="header-logo flx flx-center">
+        <!-- <a href="/" class="header-logo flx flx-center">
           <img src="/src/assets/icons/logo.svg" alt="Logo">
-        </a>
+        </a> -->
         <nav class="flx">
           <a href="/" class="header-link">Home</a>
           <a href="/#about-section" class="header-link">About</a>
