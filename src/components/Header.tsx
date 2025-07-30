db8ace7d1ad279a237eefa6f4733bc13e577d86e
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-07-30 10:28:34 -0600

    Move to TypeScript, update Money Counter metadata and picture

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { headerLinks, contacts } from "../assets/data";
-import MenuSvg from '../icons/menu.svg?react';
-import CloseSvg from '../icons/close.svg?react';
+import MenuSvg from "../icons/menu.svg?react";
+import CloseSvg from "../icons/close.svg?react";
+
+type OpenMenu = boolean;
+type SetOpenMenu = React.Dispatch<React.SetStateAction<boolean>>;
+type Children = React.ReactNode;
 
 export function Header() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -9,45 +13,70 @@ export function Header() {
   return (
     <header className="flex flex-col max-w-7xl p-7 mx-auto">
       <div className="flex w-full justify-between items-center">
-      <a href="/" className="flex items-center text-5xl text-blue-600 dark:text-blue-300 font-semibold">RM</a>
-        <HeaderNavDesktop />
+        <a
+          href="/"
+          className="flex items-center text-5xl text-blue-600 dark:text-blue-300 font-semibold"
+        >
+          RM
+        </a>
+        <HeaderNavDesktop setOpenMenu={setOpenMenu} />
         <HeaderToggle openMenu={openMenu} setOpenMenu={setOpenMenu} />
       </div>
-      {openMenu &&
+      {openMenu && (
         <HeaderNav setOpenMenu={setOpenMenu}>
           <HeaderToggle openMenu={openMenu} setOpenMenu={setOpenMenu} />
-        </HeaderNav>}
+        </HeaderNav>
+      )}
     </header>
   );
 }
 
-function HeaderToggle({ openMenu, setOpenMenu }) {
+function HeaderToggle({
+  openMenu,
+  setOpenMenu,
+}: {
+  openMenu: OpenMenu;
+  setOpenMenu: SetOpenMenu;
+}) {
   return (
     <button
       className="flex md:hidden cursor-pointer"
-      onClick={() => setOpenMenu(state => !state)}>
-      {openMenu ?
-        <CloseSvg className="size-9" /> :
+      onClick={() => setOpenMenu((state) => !state)}
+    >
+      {openMenu ? (
+        <CloseSvg className="size-9" />
+      ) : (
         <MenuSvg className="size-9" />
-      }
+      )}
     </button>
   );
 }
 
-function HeaderNavDesktop() {
+function HeaderNavDesktop({ setOpenMenu }: { setOpenMenu: SetOpenMenu }) {
   return (
     <nav className="hidden md:flex items-center gap-10">
-      <HeaderItems />
+      <HeaderItems setOpenMenu={setOpenMenu} />
     </nav>
   );
 }
 
-function HeaderNav({ children, setOpenMenu }) {
+function HeaderNav({
+  children,
+  setOpenMenu,
+}: {
+  children: Children;
+  setOpenMenu: SetOpenMenu;
+}) {
   return (
     <>
       <nav className="flex md:hidden fixed top-0 left-0 flex-col h-full w-full p-7 items-center gap-10 bg-white/90 dark:bg-neutral-800/90 backdrop-blur-2xl z-10">
         <div className="flex w-full justify-between items-center">
-          <a href="/" className="flex items-center text-5xl text-blue-600 dark:text-blue-300 font-semibold">RM</a>
+          <a
+            href="/"
+            className="flex items-center text-5xl text-blue-600 dark:text-blue-300 font-semibold"
+          >
+            RM
+          </a>
           {children}
         </div>
         <HeaderItems setOpenMenu={setOpenMenu} />
@@ -56,22 +85,24 @@ function HeaderNav({ children, setOpenMenu }) {
   );
 }
 
-function HeaderItems({ setOpenMenu }) {
+function HeaderItems({ setOpenMenu }: { setOpenMenu: SetOpenMenu }) {
   return (
     <>
       <div className="flex flex-col md:flex-row gap-5">
-        {headerLinks.map((link, index) =>
+        {headerLinks.map(({ link, name }) => (
           <a
-            href={link.link}
+            href={link}
             className="font-semibold text-center transition text-xl duration-150 text-neutral-600 dark:text-neutral-300 hover:text-indigo-600 dark:hover:text-indigo-300"
             onClick={() => setOpenMenu(false)}
-            key={index}
-          >{link.name}</a>
-        )}
+            key={name}
+          >
+            {name}
+          </a>
+        ))}
       </div>
       <div className="flex gap-5">
-        {contacts.map(({ link, Icon }, index) => (
-          <a href={link} target="_blank" rel="noopener noreferrer" key={index}>
+        {contacts.map(({ name, link, Icon }) => (
+          <a key={name} href={link} target="_blank" rel="noopener noreferrer">
             <Icon className="size-9 opacity-80 hover:scale-110 cursor-pointer transition duration-150" />
           </a>
         ))}
---
+++ src/types/header.ts
@@ -0,0 +1,2 @@
+export type StateBool = boolean;
+export type StateSet = React.Dispatch<React.SetStateAction<boolean>>;
